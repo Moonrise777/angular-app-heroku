6f3ea84405e6c15c804023f968235c0ada5aa2a6
Arely <arelyzuleika@gmail.com>
2024-06-07 18:19:45 -0600

    Cambios completos en estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-app-heroku';
+
+  data: any;
+
+  constructor(private apiService:ApiService) {}
+  ngOnInit(): void {
+    this.apiService.getData().subscribe(data => {
+      this.data = data;
+    });
+  }
 }
